c4dee5b793e38507a0bde7f82631f74ec0f2614b
Jacob Brown <Jake@insphereltd.com>
2022-03-18 16:01:54 +0000

    Added basic framework for message collator.

---
+++ JSim.Core/Common/EventCollator/IMessageCollator.cs
@@ -0,0 +1,34 @@
+﻿namespace JSim.Core.Common
+{
+    /// <summary>
+    /// Message collator to provide a mechanism for pub/sub uncoupled events.
+    /// </summary>
+    public interface IMessageCollator
+    {
+        /// <summary>
+        /// Checks to see if a message type has at least one handler subscribed.
+        /// </summary>
+        /// <typeparam name="T">Type of the message you want to check.</typeparam>
+        /// <returns>True if message type has at least one handler subscribed.</returns>
+        bool ContainsHandlerFor<T>() where T : class;
+
+        /// <summary>
+        /// Subscribes a message handler implementation to published messages.
+        /// </summary>
+        /// <param name="messageHandler">Message handler implementation for the type to subscribe to.</param>
+        void Subscribe(IMessageHandler messageHandler);
+
+        /// <summary>
+        /// Unsubscribes a message handler implementation.
+        /// </summary>
+        /// <param name="messageHandler">Message handler implementation for the type to unsubscribe from.</param>
+        void Unsubscribe(IMessageHandler messageHandler);
+
+        /// <summary>
+        /// Publishes a message to all subscribers of the message type.
+        /// </summary>
+        /// <typeparam name="T">Type of the message to publish.</typeparam>
+        /// <param name="message">Message to publish to subscribers.</param>
+        void Publish<T>(T message) where T : class;
+    }
+}
---
+++ JSim.Core/Common/EventCollator/MessageCollator.cs
@@ -0,0 +1,43 @@
+﻿namespace JSim.Core.Common
+{
+    /// <summary>
+    /// Standard implementation of a message collator.
+    /// </summary>
+    public class MessageCollator : IMessageCollator
+    {
+        public MessageCollator()
+        {
+            handlers = new List<IMessageHandler>();
+        }
+
+        public bool ContainsHandlerFor<T>() where T : class
+        {
+            return
+                handlers
+                .OfType<IMessageHandler<T>>()
+                .Count() > 0;
+        }
+        
+        public void Subscribe(IMessageHandler messageHandler)
+        {
+            handlers.Add(messageHandler);
+        }
+
+        public void Unsubscribe(IMessageHandler messageHandler)
+        {
+            handlers.Remove(messageHandler);
+        }
+
+        public void Publish<T>(T message) where T : class
+        {
+            var messageHandlers = handlers.OfType<IMessageHandler<T>>();
+
+            foreach (var messageHandler in messageHandlers)
+            {
+                messageHandler.Handle(message);
+            }
+        }
+
+        private List<IMessageHandler> handlers;
+    }
+}

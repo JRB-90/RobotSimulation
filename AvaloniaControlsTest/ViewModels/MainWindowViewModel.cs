6180cae3460476e0cd32221e7621ce2ee84f9eef
Jacob Brown <Jake@insphereltd.com>
2022-04-10 17:22:16 +0100

    Added projects for controls library and test app.

---
+++ AvaloniaControlsTest/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,9 @@
+using JSim.Avalonia.ViewModels;
+
+namespace AvaloniaControlsTest.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ JSim.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+using ReactiveUI;
+
+namespace JSim.Avalonia.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}

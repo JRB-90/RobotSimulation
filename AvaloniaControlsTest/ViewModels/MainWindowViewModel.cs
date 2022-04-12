271783f84f2d937662740620554a1ff1291450d8
Jacob Brown <Jake@insphereltd.com>
2022-04-12 21:21:23 +0100

    Added TextBox that overides the material styles and allows for data validation.

--- AvaloniaControlsTest/ViewModels/MainWindowViewModel.cs
+++ AvaloniaControlsTest/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,21 @@
 using JSim.Avalonia.ViewModels;
+using ReactiveUI;
 
 namespace AvaloniaControlsTest.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        public MainWindowViewModel()
+        {
+            doubleValue = -1234.505;
+        }
+
+        public double DoubleValue
+        {
+            get => doubleValue;
+            set => this.RaiseAndSetIfChanged(ref doubleValue, value);
+        }
+
+        private double doubleValue;
     }
 }
---
+++ JSim.Avalonia/Converters/DoubleToTruncatedStringConverter.cs
@@ -0,0 +1,55 @@
+﻿using Avalonia;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+using System.Globalization;
+
+namespace JSim.Avalonia.Converters
+{
+    public class DoubleToTruncatedStringConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is double d)
+            {
+                return $"{d:F3}";
+            }
+            else
+            {
+                return 
+                    new BindingNotification(
+                        new ArgumentException("Argument is not a double"),
+                        BindingErrorType.Error
+                    );
+            }
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is string s)
+            {
+                try
+                {
+                    double d = System.Convert.ToDouble(s);
+
+                    return d;
+                }
+                catch
+                {
+                    return
+                        new BindingNotification(
+                            new ArgumentException("Failed to parse string to double"),
+                            BindingErrorType.Error
+                        );
+                }
+            }
+            else
+            {
+                return
+                    new BindingNotification(
+                        new ArgumentException("Argument is not a string"),
+                        BindingErrorType.Error
+                    );
+            }
+        }
+    }
+}

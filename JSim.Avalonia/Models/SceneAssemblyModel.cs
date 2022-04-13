6050af88c8518b6553a73c8a08bbb57568728032
Jacob Brown <Jake@insphereltd.com>
2022-04-13 19:58:38 +0100

    Added models for scene graph.

---
+++ JSim.Avalonia/Models/SceneAssemblyModel.cs
@@ -0,0 +1,58 @@
+﻿using JSim.Core.SceneGraph;
+using ReactiveUI;
+
+namespace JSim.Avalonia.Models
+{
+    internal class SceneAssemblyModel : SceneObjectModelBase
+    {
+        readonly ISceneAssembly assembly;
+
+        public SceneAssemblyModel(ISceneAssembly assembly)
+          :
+            base(
+                assembly,
+                "Group"
+            )
+        {
+            this.assembly = assembly;
+            assembly.SceneObjectModified += OnAssemblyModified;
+        }
+
+        public IReadOnlyCollection<SceneObjectModelBase> Children =>
+            FormChildren();
+
+        public void AddAssembly()
+        {
+            assembly.CreateNewAssembly();
+        }
+
+        public void AddEntity()
+        {
+            assembly.CreateNewEntity();
+        }
+
+        private IReadOnlyCollection<SceneObjectModelBase> FormChildren()
+        {
+            var children = new List<SceneObjectModelBase>();
+
+            foreach (var sceneObject in assembly.Children)
+            {
+                if (sceneObject is ISceneAssembly assembly)
+                {
+                    children.Add(new SceneAssemblyModel(assembly));
+                }
+                else if (sceneObject is ISceneEntity entity)
+                {
+                    children.Add(new SceneEntityModel(entity));
+                }
+            }
+
+            return children;
+        }
+
+        private void OnAssemblyModified(object sender, SceneObjectModifiedEventArgs e)
+        {
+            this.RaisePropertyChanged(nameof(Children));
+        }
+    }
+}
---
+++ JSim.Avalonia/Models/SceneObjectModelBase.cs
@@ -0,0 +1,39 @@
+﻿using JSim.Core.SceneGraph;
+using ReactiveUI;
+
+namespace JSim.Avalonia.Models
+{
+    internal abstract class SceneObjectModelBase : ReactiveObject
+    {
+        readonly ISceneObject sceneObject;
+
+        public SceneObjectModelBase(
+            ISceneObject sceneObject,
+            string iconName)
+        {
+            this.sceneObject = sceneObject;
+            IconName = iconName;
+            sceneObject.SceneObjectModified += OnSceneObjectModified;
+        }
+
+        public string IconName { get; }
+
+        public string Name =>
+            sceneObject.Name;
+
+        public void Remove()
+        {
+            sceneObject.ParentAssembly?.DetachObject(sceneObject);
+        }
+
+        public void Move(ISceneAssembly assembly)
+        {
+            sceneObject.MoveAssembly(assembly);
+        }
+
+        private void OnSceneObjectModified(object sender, SceneObjectModifiedEventArgs e)
+        {
+            this.RaisePropertyChanged(nameof(Name));
+        }
+    }
+}

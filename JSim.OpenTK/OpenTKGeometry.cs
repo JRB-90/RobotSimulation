11a199ea2d98f86f6da83d1e5bb7fc14d5efcc18
Jacob Brown <Jake@insphereltd.com>
2022-04-24 17:30:54 +0100

    Added am opentk geometry representation.

---
+++ JSim.OpenTK/OpenTKGeometry.cs
@@ -0,0 +1,29 @@
+﻿using JSim.Core.Common;
+using JSim.Core.Render;
+
+namespace JSim.OpenTK
+{
+    public class OpenTKGeometry : GeometryBase
+    {
+        public OpenTKGeometry(
+            INameRepository nameRepository, 
+            IGeometryCreator creator)
+          :
+            base(
+                nameRepository,
+                creator)
+        {
+        }
+
+        public Vbo? VBO { get; private set; }
+
+        protected override void Rebuild()
+        {
+            VBO = 
+                VboUtils.CreateVbo(
+                    Vertices.ToArray(), 
+                    Indices.ToArray()
+                );
+        }
+    }
+}

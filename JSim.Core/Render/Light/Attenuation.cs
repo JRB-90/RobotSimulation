7f7d8c6a9931df98b1c9f66fbaa49093d1cb68ce
Jacob Brown <Jake@insphereltd.com>
2022-05-23 10:45:44 +0100

    Added attenuation class.

---
+++ JSim.Core/Render/Light/Attenuation.cs
@@ -0,0 +1,41 @@
+﻿namespace JSim.Core.Render
+{
+    /// <summary>
+    /// Contains a quadratic polynomial function that represents a lights
+    /// decay over distance from the source.
+    /// </summary>
+    public class Attenuation
+    {
+        public Attenuation()
+        {
+            Constant = 0.0;
+            Linear = 1.0;
+            Quadratic = 0.0;
+        }
+
+        public Attenuation(
+            double constant,
+            double linear,
+            double quadratic)
+        {
+            Constant = constant;
+            Linear = linear;
+            Quadratic = quadratic;
+        }
+
+        /// <summary>
+        /// Gets the constant attenuation component.
+        /// </summary>
+        public double Constant { get; set; }
+
+        /// <summary>
+        /// Gets the linear attenuation component.
+        /// </summary>
+        public double Linear { get; set; }
+
+        /// <summary>
+        /// Gets the quadratic attenuation component.
+        /// </summary>
+        public double Quadratic { get; set; }
+    }
+}

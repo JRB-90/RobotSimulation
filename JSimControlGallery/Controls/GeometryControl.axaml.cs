26a4e85ef88c8cba42710bd2c0fb0dba51b7340a
Jacob Brown <Jake@insphereltd.com>
2022-06-01 08:35:27 +0100

    Added extra memebers for the geometry control.

--- JSimControlGallery/Controls/GeometryControl.axaml.cs
+++ JSimControlGallery/Controls/GeometryControl.axaml.cs
@@ -7,9 +7,19 @@ namespace JSimControlGallery.Controls
 {
     public partial class GeometryControl : UserControl
     {
+        readonly CheckBox isVisibleCheckBox;
+        readonly CheckBox isHighlightedCheckBox;
+
         public GeometryControl()
         {
             InitializeComponent();
+
+            isVisibleCheckBox = this.FindControl<CheckBox>("IsVisibleCheckBox");
+            isHighlightedCheckBox = this.FindControl<CheckBox>("IsHighlightedCheckBox");
+            isVisibleCheckBox.PropertyChanged += OnIsVisibleChanged;
+            isHighlightedCheckBox.PropertyChanged += OnIsHighlightedChanged;
+
+            MaterialControl = new MaterialControl() { };
         }
 
         public static readonly DirectProperty<GeometryControl, IGeometry?> GeometryProperty =
@@ -25,11 +35,7 @@ namespace JSimControlGallery.Controls
             set
             {
                 SetAndRaise(GeometryProperty, ref geometry, value);
-                if (geometry != null)
-                {
-                    RaisePropertyChanged(GeometryNameProperty, "", geometry.Name);
-                    MaterialControl = new MaterialControl() { Material = geometry.Material };
-                }
+                UpdateDisplayedValues();
             }
         }
 
@@ -41,6 +47,15 @@ namespace JSimControlGallery.Controls
 
         public string? GeometryName =>
             Geometry?.Name;
+
+        public static readonly DirectProperty<GeometryControl, string?> GeometryIDProperty =
+            AvaloniaProperty.RegisterDirect<GeometryControl, string?>(
+                nameof(GeometryID),
+                o => o.GeometryID
+            );
+
+        public string? GeometryID =>
+            Geometry?.ID.ToString();
 
         public static readonly DirectProperty<GeometryControl, MaterialControl?> MaterialControlProperty =
             AvaloniaProperty.RegisterDirect<GeometryControl, MaterialControl?>(
@@ -57,6 +72,42 @@ namespace JSimControlGallery.Controls
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private void UpdateDisplayedValues()
+        {
+            if (geometry != null)
+            {
+                RaisePropertyChanged(GeometryNameProperty, "", geometry.Name);
+                RaisePropertyChanged(GeometryIDProperty, "", geometry.ID.ToString());
+                isVisibleCheckBox.IsChecked = geometry.IsVisible;
+                isHighlightedCheckBox.IsChecked = geometry.IsHighlighted;
+                MaterialControl = new MaterialControl() { Material = geometry.Material };
+            }
+        }
+
+        private void OnIsVisibleChanged(object? sender, AvaloniaPropertyChangedEventArgs e)
+        {
+            if (e.Property.Name == nameof(CheckBox.IsChecked))
+            {
+                if (Geometry!= null &&
+                    isVisibleCheckBox.IsChecked != null)
+                {
+                    Geometry.IsVisible = isVisibleCheckBox.IsChecked.Value;
+                }
+            }
+        }
+
+        private void OnIsHighlightedChanged(object? sender, AvaloniaPropertyChangedEventArgs e)
+        {
+            if (e.Property.Name == nameof(CheckBox.IsChecked))
+            {
+                if (Geometry != null &&
+                    isHighlightedCheckBox.IsChecked != null)
+                {
+                    Geometry.IsVisible = isHighlightedCheckBox.IsChecked.Value;
+                }
+            }
         }
 
         private IGeometry? geometry;
---
+++ JSimControlGallery/Design/DesignData.cs
@@ -0,0 +1,54 @@
+﻿using Castle.Facilities.TypedFactory;
+using Castle.Windsor;
+using JSim.BasicBootstrapper;
+using JSim.Core;
+using JSim.Core.Render;
+using JSim.Core.SceneGraph;
+using JSim.Logging;
+
+namespace JSimControlGallery.Design
+{
+    internal static class DesignData
+    {
+        static DesignData()
+        {
+            IWindsorContainer container = BootstrapContainer();
+            ISimApplication app = container.Resolve<ISimApplication>();
+            ISceneManager sceneManager = app.SceneManager;
+            IScene scene = sceneManager.CurrentScene;
+            ILogger logger = container.Resolve<ILogger>();
+
+            ISceneEntity entity = scene.Root.CreateNewEntity("Entity");
+            Geometry = entity.GeometryContainer.Root.CreateChildGeometry("TestGeometry");
+
+            Material =
+                new Material(
+                    new Color(1.0f, 0.3f, 0.2f, 0.1f),
+                    new Color(1.0f, 0.8f, 0.7f, 0.5f),
+                    new Color(1.0f, 0.1f, 0.9f, 0.7f),
+                    0.7,
+                    ShadingType.Smooth
+                );
+
+            Geometry.Material = Material;
+        }
+
+        public static IMaterial Material { get; }
+
+        public static IGeometry Geometry { get; }
+
+        private static IWindsorContainer BootstrapContainer()
+        {
+            var container = new WindsorContainer();
+            container.AddFacility<TypedFactoryFacility>();
+            container.Install(
+                new BasicApplicationInstaller(),
+                Log4NetInstaller.FromEmbedded("log4netconsole.config"),
+                new BasicSceneManagerInstaller(),
+                new DummyRenderingManagerInstaller()
+            );
+
+            return container;
+        }
+    }
+}

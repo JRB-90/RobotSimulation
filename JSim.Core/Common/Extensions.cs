fafc91080b1d87c1cfc14e71df70a26c27940a46
Jacob Brown <Jake@insphereltd.com>
2022-06-04 21:22:53 +0100

    Added a flatten helper extension for linq.

---
+++ JSim.Core/Common/Extensions.cs
@@ -0,0 +1,12 @@
+﻿namespace JSim.Core.Common
+{
+    public static class Extensions
+    {
+        public static IEnumerable<T> Flatten<T>(
+            this IEnumerable<T> e,
+            Func<T, IEnumerable<T>> f)
+        {
+            return e.SelectMany(c => f(c).Flatten(f)).Concat(e);
+        }
+    }
+}
